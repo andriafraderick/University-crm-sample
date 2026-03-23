a64aaebf1dfab3633dc7486765cc8e9cdf9b2b4c
andriafraderick <andriafraderick02@gmail.com>
2026-03-23 11:35:23 +0530

    Fix STATIC_ROOT for collectstatic on Render

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -127,7 +127,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Allow React dev server to talk to Django
 # CORS_ALLOWED_ORIGINS = [
